dc120f1520738331bf2efe7dc971b1b7cd532d66
Warsha97 <warkiringoda@gmail.com>
2025-06-10 01:11:17 +0200

    Adding EFCore dbcontext

---
+++ MyTestDotnetApp/Data/GameStoreContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyTestDotnetApp.Entities;
+
+namespace MyTestDotnetApp.Data
+{
+    public class GameStoreContext(DbContextOptions<GameStoreContext> options) 
+        : DbContext(options)
+    {
+        public DbSet<Game> Games => Set<Game>();
+        public DbSet<Genre> Genres => Set<Genre>();
+    }
+}
