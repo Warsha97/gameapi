186d0a66afc6ede2a41e62075b03a249c2cf01da
Warsha97 <warkiringoda@gmail.com>
2025-06-17 20:32:54 +0200

    test comment

--- MyTestDotnetApp/Program.cs
+++ MyTestDotnetApp/Program.cs
@@ -4,6 +4,7 @@ using MyTestDotnetApp.Endpoints;
 var builder = WebApplication.CreateBuilder(args);
 //This is where we configure this web application with all
 //services, middlewear etc.
+// test
 var connString = builder.Configuration.GetConnectionString("GameStore");
 builder.Services.AddSqlite<GameStoreContext>(connString);
 
